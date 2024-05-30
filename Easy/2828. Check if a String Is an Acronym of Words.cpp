b011a9153b7180a012910b72c226b3854e0803b8
Vladislav Bobovoz <vladislav.bobovoz@outlook.com>
2024-05-30 22:29:55 +0300

    30.05.2024

---
+++ Easy/2828. Check if a String Is an Acronym of Words.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if(s.size() != words.size()) {
+            return false;
+        }
+        for(int i = 0; i < words.size(); ++i) {
+            if(s[i] != words[i][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
